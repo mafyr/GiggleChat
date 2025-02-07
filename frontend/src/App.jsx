c6998a56fc20628094551a0a503a05d03a3ff145
mafyr-webdev <mafyr1027@gmail.com>
2025-02-07 22:30:23 +0500

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/Login";
-import Signup from "./pages/signup/signup";
+import Signup from "./pages/signup/Signup"; // Fixed capitalization
 import Home from "./pages/home/Home";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "../src/context/AuthContext";
 
 function App() {
   const { authUser } = useAuthContext();
+
   return (
     <div>
       <Routes>
-       <Route path='/' element={authUser ? <Home/> : <Navigate to={"/Login"}/>}/>
-       <Route path="/login" element={authUser ? <Navigate to='/'/> : <Login/>}  />
-       <Route path="/signup" element={authUser ? <Navigate to='/'/> : <Signup/> }  />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
       </Routes>
       <Toaster />
     </div>
