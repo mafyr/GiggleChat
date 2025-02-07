5ae704604216727870748aade6f89e2c134a303d
mafyr-webdev <mafyr1027@gmail.com>
2025-02-07 21:45:26 +0500

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// bcz of line 28 - 32, we will be able to run frontend from our server
+
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")); //this means go inside frontend inside dist and than execute index.html
 });
 
 server.listen(PORT, () => {
--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -18,19 +18,22 @@ const MessageInput = () => {
 			<div className='w-full relative'>
 				<input
 					type='text'
-					className='border text-sm rounded-lg block w-full p-3 outline-none  bg-white text-black'
+					className='border text-sm rounded-lg block w-full p-3 outline-none text-black'
 					placeholder='Send a message'
 					value={message}
 					onChange={(e) => setMessage(e.target.value)}
 				/>
-				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-					{loading ? <div className='loading loading-spinner'></div> : <BsSend />}
+				<button type='submit' className=' absolute inset-y-0 end-0 flex items-center pe-3'>
+					{loading ? <div className='loading loading-spinner text-black'></div> : <BsSend className="text-black"/>}
+
 				</button>
 			</div>
 		</form>
 	);
 };
+
 export default MessageInput;
+
 
 // STARTER CODE SNIPPET
 // import { BsSend } from "react-icons/bs";
