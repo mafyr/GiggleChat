fb996ead6934c89fbfb229ab195d259ab6dfa2d8
mafyr-webdev <mafyr1027@gmail.com>
2025-02-08 15:53:07 +0500

    updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://foolish-ailis-gigglechatmafyr-b522f9e2.koyeb.app", {
+      const socket = io("http://localhost:5000", {
         query: {
           userId: authUser._id,
         },
